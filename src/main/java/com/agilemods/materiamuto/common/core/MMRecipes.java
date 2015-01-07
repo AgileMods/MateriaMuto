67844dcd8412f0a1224c7817fe109cd4eff1e469
dmillerw <dmillerw@gmail.com>
2015-01-06 17:33:38 -0800

    Make dust recipes actually give 40 dust

--- src/main/java/com/agilemods/materiamuto/common/core/MMRecipes.java
+++ src/main/java/com/agilemods/materiamuto/common/core/MMRecipes.java
@@ -1,6 +1,5 @@
 package com.agilemods.materiamuto.common.core;
 
-import com.agilemods.materiamuto.common.core.MMItems;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
@@ -9,9 +8,9 @@ import net.minecraft.item.ItemStack;
 public class MMRecipes {
 
     public static void initialize() {
-        ItemStack dustLow = new ItemStack(MMItems.covalenceDust, 1, 0);
-        ItemStack dustMedium = new ItemStack(MMItems.covalenceDust, 1, 1);
-        ItemStack dustHigh = new ItemStack(MMItems.covalenceDust, 1, 2);
+        ItemStack dustLow = new ItemStack(MMItems.covalenceDust, 40, 0);
+        ItemStack dustMedium = new ItemStack(MMItems.covalenceDust, 40, 1);
+        ItemStack dustHigh = new ItemStack(MMItems.covalenceDust, 40, 2);
 
         GameRegistry.addShapelessRecipe(
                 dustLow,
